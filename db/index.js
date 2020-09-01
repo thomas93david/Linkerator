e1dfb3625c761557775f9ade25063921e25116cf
David Thomas <your_email@real_email.edu>
2020-08-31 20:56:13 -0400

    Changes to init.db

--- db/index.js
+++ db/index.js
@@ -1,13 +1,15 @@
 // Connect to DB
-const { Client } = require('pg');
-const DB_NAME = 'Linkerator'
-const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${ DB_NAME }`;
+const { Client } = require("pg");
+const DB_NAME = "Linkerator";
+const DB_URL =
+  process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 const client = new Client(DB_URL);
 
 // database methods
+// future methods
 
 // export
 module.exports = {
   client,
   // db methods
-}
+};
--- db/init_db.js
+++ db/init_db.js
@@ -1,8 +1,8 @@
 // code to build and initialize DB goes here
 const {
-  client
-  // other db methods 
-} = require('./index');
+  client,
+  // other db methods
+} = require("./index");
 
 async function buildTables() {
   try {
@@ -11,7 +11,6 @@ async function buildTables() {
     // drop tables in correct order
 
     // build tables in correct order
-
   } catch (error) {
     throw error;
   }
